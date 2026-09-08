4ba4125a4faf540974ad4b01d7c98b097ac276a3
Serhan KARAKOÇ <serhankarakoc@gmail.com>
2026-09-08 23:19:54 +0300

    Move reference application to zatrano/examples

--- examples/reference/doc.go
+++
@@ -1,6 +0,0 @@
-// Package reference is a production-shaped ZATRANO consumer used to pressure
-// the public lifecycle, configuration, HTTP, and worker contracts.
-//
-// It is not a framework runtime package. It must not import
-// github.com/zatrano/packages (this module's architecture tests forbid it).
-package reference
